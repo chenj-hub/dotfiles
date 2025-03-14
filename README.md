83a3432169638556474abdac5abfa9f3dffe8c53
Jackie <meisjackie@gmail.com>
2025-03-14 08:09:08 -0400

    Refactor dotfiles to use ansible

--- README.md
+++ README.md
@@ -1,29 +1,21 @@
 # dotfiles setup 
 
-## Setup
-```sh
-git init --bare $HOME/.dotfiles
-alias d='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-d remote add origin @git_link@
+## Run ansible locally
+
+```
+./ansible_wrapper.sh ansible-playbook dotfiles.yml --ask-become-pass
 ```
 
-## Replication
-```sh
-git clone --separate-git-dir=$HOME/.dotfiles @git_link@ dotfiles-tmp
-rsync --recursive --verbose --exclude '.git' dotfiles-tmp/ $HOME/
-rm -r dotfiles-tmp
+## Development
+
+Use ansible-lint for styling
+
+```
+./ansible_wrapper.sh ansible-lint autofix
 ```
 
-## Configuration
-```sh
-d config status.showUntrackedFiles no
-d remote set-url origin @git_link@
-```
+Update requirements.txt
 
-## Usage Example
-```sh
-d status
-d add .gitconfig
-d commit -m 'Add gitconfig'
-d push
+```
+uv pip freeze > requirements.txt 
 ```
---
+++ ansible_wrapper.sh
@@ -0,0 +1,36 @@
+##!/bin/bash
+
+set -eu -o pipefail
+
+if [[ $OSTYPE == darwin ]]; then
+  command -v brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! command -v uv &> /dev/null; then
+  if [[ $OSTYPE == darwin* ]]; then
+    brew install uv
+  else
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+  fi
+  source $HOME/.local/bin/env
+else
+  echo "uv is already installed"
+fi
+
+PYTHON_VERSION=3.12.3
+DOT_ANSIBLE_VENV=$HOME/.dot_ansible_venv
+
+if  python3 -V | grep -q $PYTHON_VERSION; then
+  echo "Python $PYTHON_VERSION is already installed"
+else
+  uv python install $PYTHON_VERSION
+fi
+
+if [[ ! -d $DOT_ANSIBLE_VENV ]]; then
+  uv venv $DOT_ANSIBLE_VENV --python $PYTHON_VERSION
+fi
+
+source $DOT_ANSIBLE_VENV/bin/activate
+uv pip install -r requirements.txt
+
+"$@"
