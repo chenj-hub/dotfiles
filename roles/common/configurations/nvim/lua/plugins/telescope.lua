9a95c48b88416b4a8ef941a12acd8117db208d1a
Jackie Springstead-Chen <jackies@squareup.com>
2025-09-29 14:37:44 -0400

    search hidden files too

--- roles/common/configurations/nvim/lua/plugins/telescope.lua
+++ roles/common/configurations/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,13 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
 
   config = function()
-    require('telescope').setup({})
+    require('telescope').setup({
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+    })
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
