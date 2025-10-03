34499f105ba13a92d0d06aea454b84aa2b7f716b
Jackie Springstead-Chen <jackies@squareup.com>
2025-10-03 18:00:17 -0400

    fix format

--- roles/common/configurations/nvim/lua/plugins/neo-tree.lua
+++ roles/common/configurations/nvim/lua/plugins/neo-tree.lua
@@ -19,7 +19,7 @@ return {
     },
   },
   config = function()
-    require('neo-tree').setup()
+    require('neo-tree').setup({})
     vim.keymap.set('n', '<leader>t', '<cmd>Neotree toggle<CR>', { noremap = true, silent = true })
     vim.keymap.set('n', '<leader>r', '<cmd>Neotree current reveal_force_cwd<CR>', { noremap = true, silent = true })
   end
