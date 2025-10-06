dc258656a004d839f374c486f3a466aed48036f4
Jackie Springstead-Chen <jackies@squareup.com>
2025-10-06 14:28:32 -0400

    use minuet-ai instead of copilot

--- roles/work/configurations/nvim/lua/plugins/work-ai.lua
+++ roles/work/configurations/nvim/lua/plugins/work-ai.lua
@@ -46,6 +46,63 @@ return {
     end
   },
   {
-    'github/copilot.vim',
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'minuet' },
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+    end,
+  },
+  {
+    'milanglacier/minuet-ai.nvim',
+    dependencies = {
+      'nvim-cmp',
+    },
+    config = function()
+      require('minuet').setup({
+        provider = 'claude',
+        provider_options = {
+          claude = {
+            model = 'claude-sonnet-4-5-20250929',
+            api_key = function ()
+              return vim.fn.system("security find-generic-password -a Anthropic -s Claude -w"):gsub("\n", "")
+            end,
+          },
+        },
+      })
+    end,
   }
 }
