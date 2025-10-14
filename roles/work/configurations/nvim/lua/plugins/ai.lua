69edd066b3efd517403b9cd1fbca71a5cc77835a
Jackie Springstead-Chen <jackies@squareup.com>
2025-10-14 16:58:16 -0400

    Swap back to claude-code

--- roles/work/configurations/nvim/lua/plugins/ai.lua
+++ roles/work/configurations/nvim/lua/plugins/ai.lua
@@ -20,19 +20,15 @@ return {
     }
   },
   {
-    "azorng/goose.nvim",
-    config = function()
-      require("goose").setup({})
-    end,
+    "greggh/claude-code.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          anti_conceal = { enabled = false },
-        },
-      }
     },
+    config = function()
+      require("claude-code").setup()
+
+      vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', { desc = 'Toggle Claude Code' })
+    end
   },
   {
     'hrsh7th/nvim-cmp',
