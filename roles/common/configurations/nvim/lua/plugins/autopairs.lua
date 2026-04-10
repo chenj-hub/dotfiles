de3e973508bc58171e621fb367706e3a0c1f54bd
Jackie <chen.jackie.k@gmail.com>
2026-04-10 07:12:45 -0400

    Move around some shortcuts

--- roles/common/configurations/nvim/lua/plugins/autopairs.lua
+++ roles/common/configurations/nvim/lua/plugins/autopairs.lua
@@ -20,6 +20,47 @@ return {
       highlight_grey = 'LineNr',
     },
   },
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        performance = { fetching_timeout = 2000 },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+    end,
+  },
   config = function(_, opts)
     require("nvim-autopairs").setup(opts)
 
@@ -31,3 +72,4 @@ return {
     end
   end,
 }
+
--- roles/work/configurations/nvim/lua/plugins/ai.lua
+++ roles/work/configurations/nvim/lua/plugins/ai.lua
@@ -116,34 +116,6 @@ return {
       cmp.setup({
         mapping = cmp.mapping.preset.insert({
           ['<C-y>'] = require('minuet').make_cmp_map(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        performance = { fetching_timeout = 2000 },
-        sources = cmp.config.sources({
-          { name = 'copilot' },
-          { name = 'minuet' },
-          { name = 'nvim_lsp' },
-          { name = 'buffer' },
-          { name = 'path' },
-        }),
       })
     end,
   },
